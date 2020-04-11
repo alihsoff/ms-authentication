cef1a76eb87b95e959ffc4fd664e5e097a2cee69
movsumova.nigar1@gmail.com <movsumova.nigar1@gmail.com>
2020-04-11 06:57:17 +0400

    changed basepackage for swagger

--- src/main/java/az/covid/msauthentication/config/SwaggerConfig.java
+++ src/main/java/az/covid/msauthentication/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("az.gdg.msauth"))
+                .apis(RequestHandlerSelectors.basePackage("az.covid.msauthentication"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiDetails());
